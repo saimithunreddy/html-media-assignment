f769cf1823a57ad36e1e4b2b0faf256fcd377e09
saimithunreddy <159929766+saimithunreddy@users.noreply.github.com>
2024-02-13 21:08:06 +0530

    select assignment files are added

---
+++ Untitled-7.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int r = 10;
+    float pie = 3.14;
+    float circumference;
+    circumference = float(2 * pie * r);
+    cout << circumference;
+    return 0;
+}
